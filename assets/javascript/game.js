ba6a9180d647edb6d936b4d3a4ba6ec696c48328
cesar avina <cesaravina@gmail.com>
2017-02-22 18:45:38 -0800

    almost done with logit

---
+++ assets/javascript/game.js
@@ -0,0 +1,112 @@
+// Global variables
+// ==========================================================
+// Arrays and variables
+var wordOptions = ["matt", "kim", "tom", "travis", "chino", "davey"];
+var selectedWord = "";
+var lettersinWord = [];
+var numBlanks = 0;
+var blanksAndSuccesses = [];
+var wrongGuesses = [];
+
+// Game Counters
+var winCount = 0;
+var lossCount = 0;
+var guessesLeft = 9;
+
+// Functions
+// ==========================================================
+function startGame () {
+	selectedWord = wordOptions[Math.floor(Math.random()*wordOptions.length)];
+	lettersinWord = selectedWord.split("")
+	numBlanks = lettersinWord.length;
+
+	// Reset
+	guessesLeft = 9;
+	wrongLetters = [];
+	blanksAndSuccesses = [];
+
+	// Populate blanks and successes with right number of blanks
+	for (var i = 0; i<numBlanks; i++) {
+		blanksAndSuccesses.push("_")
+		}
+
+	// Change HTML to reflect game conditions
+	document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join(" ");
+	document.getElementById("numGuesses").innerHTML = guessesLeft;
+	document.getElementById("winCounter").innerHTML = winCount;
+	document.getElementById("lossCounter").innerHTML = lossCount;
+
+	// testing/debugging
+	console.log(selectedWord);
+	console.log(lettersinWord)
+	console.log(numBlanks)
+	console.log(blanksAndSuccesses)
+}	
+
+function checkLetters (letter) {
+	// check if letter is in code
+	var isLetterInWord = false;
+	for (var i=0; i<numBlanks; i++) {
+		if(selectedWord[i] == letter) {
+			isLetterInWord = true;
+			alert ("letter found!")
+		}
+	}
+
+// check where in the word the letter exits
+	for (var i = 0; i<numBlanks; i++){
+		if (selectedWord[i] == letter){
+			blanksAndSuccesses[i] = letter;
+		}
+		// letter wasn't found
+	else {
+		wrongLetters.push(letter);
+		guessesLeft --;
+	}
+	console.log(blanksAndSuccesses)
+}
+
+
+}
+function roundComplete(){
+	console.log("Win Count: " + winCount + " | Loss Count: " + lossCount + " | Guesses Left: " + guessesLeft)
+}
+
+// Main Process
+// ==========================================================
+// initiates code the first time
+
+startGame();
+
+// keyclicks
+document.onkeyup = function (event) {
+	var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+	checkLetters(letterGuessed);
+	roundComplete();
+	
+	// Testing/debugging
+	console.log(letterGuessed)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
